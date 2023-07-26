3d1e59bfdade7a1a436f189034bd582df2e0b580
Kwaku Tutu <65915855+KwakuTutu@users.noreply.github.com>
2023-07-26 09:34:57 +0000

    size of hamburger menu and smooth scrolling effect

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ const nav = document.querySelector('.nav');
 navToggle.addEventListener('click', () =>{
         nav.classList.toggle('nav--visible');
 })
+
+document.addEventListener('DOMContentLoaded', function() {
+        const links = document.querySelectorAll('.section-link');
+      
+        links.forEach(link => {
+          link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const target = document.querySelector(this.getAttribute('href'));
+            const headerOffset = 30;
+            const elementPosition = target.getBoundingClientRect().top;
+            const offsetPosition = elementPosition - headerOffset;
+      
+            window.scrollBy({
+              top: offsetPosition,
+              behavior: 'smooth'
+            });
+          });
+        });
+      });
+      
